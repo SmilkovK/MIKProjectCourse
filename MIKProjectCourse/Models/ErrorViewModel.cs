0fa8d0d4ec556f9468b5b9fbfd710828cd201a0a
SmilkovK <kire.smilkov@students.finki.ukim.mk>
2025-04-26 12:59:28 +0200

    Add project files.

---
+++ MIKProjectCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIKProjectCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
