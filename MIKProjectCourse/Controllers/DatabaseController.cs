fcd3fecf85119ea8a2bff928060b7c31397d033b
SmilkovK <kire.smilkov@students.finki.ukim.mk>
2025-05-05 19:29:09 +0200

    Database Tab

---
+++ MIKProjectCourse/Controllers/DatabaseController.cs
@@ -0,0 +1,37 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace MIKProjectCourse.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult WhyDatabase()
+        {
+            return View();
+        }
+        public IActionResult TypesOfDatabase()
+        {
+            return View();
+        }
+        public IActionResult HowDatabaseWorks()
+        {
+            return View();
+        }
+        public IActionResult RelationalDatabase()
+        {
+            return View();
+        }
+        public IActionResult Example()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+
+    }
+}
