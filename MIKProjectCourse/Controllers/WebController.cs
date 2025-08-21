9d9727d8a033a67a3993edd7de434d2cbb087d82
Kire Smilkov <kiresmilkov14@gmail.com>
2025-08-21 17:10:58 +0200

    Web Part 1

---
+++ MIKProjectCourse/Controllers/WebController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIKProjectCourse.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Instalation()
+        {
+            return View();
+        }
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+        public IActionResult Controllers()
+        {
+            return View();
+        }
+        public IActionResult ControllersExample()
+        {
+            return View();
+        }
+        public IActionResult Models()
+        {
+            return View();
+        }
+        public IActionResult ModelsExample()
+        {
+            return View();
+        }
+        public IActionResult Views()
+        {
+            return View();
+        }
+        public IActionResult ViewsExample()
+        {
+            return View();
+        }
+    }
+}
