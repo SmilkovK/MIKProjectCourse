8a750bbef2da9c50af3dedb9479dfe08ac0e6059
Kire Smilkov <kiresmilkov14@gmail.com>
2025-08-22 23:20:27 +0200

    Setting up part 2 and refresh fix

---
+++ MIKProjectCourse/Controllers/OOPController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIKProjectCourse.Controllers
+{
+    public class OOPController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+        public IActionResult Classes()
+        {
+            return View();
+        }
+        public IActionResult Constructors()
+        {
+            return View();
+        }
+        public IActionResult Encapsulation()
+        {
+            return View();
+        }
+        public IActionResult Polymorphism()
+        {
+            return View();
+        }
+        public IActionResult Interfaces()
+        {
+            return View();
+        }
+        public IActionResult Objects()
+        {
+            return View();
+        }
+        public IActionResult Abstraction()
+        {
+            return View();
+        }
+        public IActionResult StaticReadonlyConst()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ MIKProjectCourse/Controllers/WebCommunicationController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MIKProjectCourse.Controllers
+{
+    public class WebCommunicationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Introduction()
+        {
+            return View();
+        }
+        public IActionResult Client()
+        {
+            return View();
+        }
+        public IActionResult Server()
+        {
+            return View();
+        }
+        public IActionResult Browser()
+        {
+            return View();
+        }
+        public IActionResult HttpPart1()
+        {
+            return View();
+        }
+        public IActionResult HttpPart2()
+        {
+            return View();
+        }
+        public IActionResult ClientSide()
+        {
+            return View();
+        }
+        public IActionResult ServerSide()
+        {
+            return View();
+        }
+        public IActionResult API()
+        {
+            return View();
+        }
+        public IActionResult Summary()
+        {
+            return View();
+        }
+    }
+}
