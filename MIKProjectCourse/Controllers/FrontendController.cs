771385d9c9c76f4d3723e009c4ba514101043ce0
DULE <DULE@DESKTOP-C87RUUH>
2025-09-04 13:43:20 +0200

    Frontend

---
+++ MIKProjectCourse/Controllers/FrontendController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace MIKProjectCourse.Controllers
+{
+    public class FrontendController : Controller
+    {
+
+        public IActionResult WhatIsFrontend()
+        {
+            return View();
+        }
+        public IActionResult WhyFrontend()
+        {
+            return View();
+        }
+        public IActionResult FrontendTechnologies()
+        {
+            return View();
+        }        
+        public IActionResult CreateWebPage()
+        {
+            return View();
+        }
+        public IActionResult WebWorks()
+        {
+            return View();
+        }
+       public IActionResult GettingtoKnowHTML()
+        {
+            return View();
+        }
+        public IActionResult SemanticsOverview()
+        {
+            return View();
+        }
+        public IActionResult IdentifyingDivisionsSpans()
+        {
+            return View();
+        }
+        public IActionResult BlockInlineElements()
+        {
+            return View();
+        }
+        public IActionResult CommentsWithinHTMLCSS()
+        {
+            return View();
+        }
+        public IActionResult textBasedElements()
+        {
+            return View();
+        }
+        public IActionResult Headings()
+        {
+            return View();
+        }
+        public IActionResult Paragraphs()
+        {
+            return View();
+        }
+        public IActionResult BoldText()
+        {
+            return View();
+        }
+        public IActionResult ItalicizeText()
+        {
+            return View();
+        }
+        public IActionResult BuildingStructure()
+        {
+            return View();
+        }
+        public IActionResult Header()
+        {
+            return View();
+        }
+        public IActionResult Navigation()
+        {
+            return View();
+        }
+        public IActionResult Article()
+        {
+            return View();
+        }
+        public IActionResult Section()
+        {
+            return View();
+        }
+        public IActionResult DecidingBetween()
+        {
+            return View();
+        }
+        public IActionResult Aside()
+        {
+            return View();
+        }
+        public IActionResult Footer()
+        {
+            return View();
+        }
+        public IActionResult InPractice()
+        {
+            return View();
+        }
+
+
+    }
+
+}
